2ae71b9b981d5dc13e380b13028803b285a9817c
Arunnirthana Arulanantham <arunnirthana@gmail.com>
2020-10-23 20:40:26 +0530

    Changes

---
+++ ArunServiceStation/Models/Provider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ArunServiceStation.Models
+{
+    public class DropDown
+    {
+        
+          
+            public Provider Provider { get; set; }
+        public TimeSlot TimeSlot { get; set; }
+    }
+
+        public enum Provider
+        {
+            Benze,
+            Toyoto,
+            Tesla,
+            Honda,
+            BMW,
+
+        }
+    public enum TimeSlot
+    {
+        "1-2",
+        2,
+        Tesla,
+        Honda,
+        BMW,
+
+    }
+}
